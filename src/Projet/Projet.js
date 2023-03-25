5bb53dd37a04c149cfc9d67f8f99f0618ec61e35
“antonio150” <antoniorollande@gmail.com>
2023-03-25 15:16:32 +0300

    first commit 2

--- src/Projet/Projet.js
+++ src/Projet/Projet.js
@@ -9,6 +9,8 @@ import page from "./img/page.JPG"
 import bpm from "./img/bpm.png"
 import miniLi from "./img/miniLi.JPG"
 
+import { AiFillGithub } from 'react-icons/ai'
+
 const Projet = () => {
   return (
     <div class="projet" id="projet">
@@ -19,6 +21,11 @@ const Projet = () => {
                 <div class="title_projet"> Chatboat simple</div>
                 <img id="chatboat_img" alt="chatboat" src={chatboat} />
 
+                <div id='site_link_div'>
+                        <div><a id='site_link' href='http://chatboat.5.youpihost.fr/'>Voir <b>Demo</b></a></div>
+                        
+                </div>
+
         </div>
 
       
@@ -26,16 +33,55 @@ const Projet = () => {
         <div>
                 <div class="title_projet"> Modification de theme <b>Prestashop</b></div>
                 <img id="chatboat_img" alt="prestashop" src={prestashop} />
+                <div>
+                       
+                                <a id='site_link' href='https://wordpresstest.5.youpihost.fr/presta17'>Voir <b>Demo</b></a>
+                                {/* <li><a id='react_link' href='https://github.com/antonio150/navira-website-test-2'>Voir <b>Répositorie</b> <AiFillGithub /></a></li> */}
+                       
+                </div>
 
         </div>
 
         <div >
                 <div class="title_projet"> Projet <b>React</b></div>
                 <div class="react_content">
-                        <img id="react" src={porftfolio} alt="portfolio"/>
-                        <img id="react" src={entreprise} alt="entreprise" />
-                        <img id="react" src={pizza} alt="pizza" />
-                        <img id="react" src={page} alt="page" />
+                        <div id='react_div'>
+                                <img id="react" src={porftfolio} alt="portfolio"/>
+
+                                <ul>
+                                        <li><a id='react_link' href='https://navira-website-test-2.vercel.app/'>Voir <b>Demo</b></a></li>
+                                        <li><a id='react_link' href='https://github.com/antonio150/navira-website-test-2'>Voir <b>Répositorie</b> <AiFillGithub /></a></li>
+                                </ul>
+                                
+                                
+                        </div>
+                        
+                        <div id='react_div'>
+                                <img id="react" src={entreprise} alt="entreprise" />
+
+                                <ul>
+                                        <li><a id='react_link' href='https://navira-website-test-1-f2v7.vercel.app/'>Voir <b>Demo</b></a></li>
+                                        <li><a id='react_link' href='https://github.com/antonio150/navira-website-test-1'>Voir <b>Répositorie</b> <AiFillGithub /></a></li>
+                                </ul>
+                        </div>
+
+                        <div id='react_div'>
+                                <img id="react" src={pizza} alt="pizza" />
+
+                                <ul>
+                                        <li><a id='react_link' href='blob:https://vercel.com/b3b8c34c-459f-4738-8ad5-41bcd11a07ef'>Voir <b>Demo</b></a></li>
+                                        <li><a id='react_link' href='https://github.com/antonio150/navira-pizza'>Voir <b>Répositorie</b> <AiFillGithub /></a></li>
+                                </ul>
+                        </div>
+
+                        <div id='react_div'>
+                                <img id="react" src={page} alt="page" />
+
+                                <ul>
+                                        <li><a id='react_link' href='blob:https://vercel.com/b74f1555-350f-4f3c-be87-3ea174ca0b02'>Voir <b>Demo</b></a></li>
+                                        <li><a id='react_link' href='https://github.com/antonio150/navira-website-test-3'>Voir <b>Répositorie</b> <AiFillGithub /></a></li>
+                                </ul>
+                        </div>
                         {/* <img id="react" src={xcams} /> */}
                 </div>
 
@@ -55,6 +101,11 @@ const Projet = () => {
                 <div>
                         <img id="chatboat_img" src={bpm} alt="bpm" />
                 </div>
+
+                <div>
+                <a id='site_link' href='https://github.com/antonio150/bpmn'>Voir <b>Répositorie</b> <AiFillGithub /></a>
+                </div>
+                        
 
                 <div>
                         <div id="description_bpm">Déscription de projet Business Process Management</div>
