7b8911090a5d285a7757c10db8ae8c12d78e8274
“antonio150” <antoniorollande@gmail.com>
2023-03-30 11:02:43 +0300

    first commit 5

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./Footer.css"
+import {BsFillTelephoneFill} from "react-icons/bs"
+import {GrMail} from "react-icons/gr"
 
 const Footer = () => {
   return (
     <div id="footer">
+      <div>
+        <BsFillTelephoneFill/> : 033 92 333 21
+      </div>
+
+      <div>
+        <GrMail/> : antoniorollande@gmail.com
+      </div>
       Copyright © 2023 by Navira | Tous droit reserver
+
+      
     </div>
   )
 }
